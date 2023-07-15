acd174ca82d6ef639986d82a54db89327a5a6b5d
Karl Kreiner <kaykay2306@gmail.com>
2023-07-15 14:49:48 +0200

    Removed myst_parser

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '0.10.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+#extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = []
